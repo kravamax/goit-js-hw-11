ad108660263ca184979ed764d16fde6153bd470e
Maksym Kravchenko <maks.cross@gmail.com>
2022-04-11 20:11:21 +0300

    Upd files

---
+++ src/index.js
@@ -0,0 +1,15 @@
+// // Описан в документации
+// import SimpleLightbox from 'simplelightbox';
+// // Дополнительный импорт стилей
+// import 'simplelightbox/dist/simple-lightbox.min.css';
+
+console.log('index.js');
+
+// const { height: cardHeight } = document
+//   .querySelector('.gallery')
+//   .firstElementChild.getBoundingClientRect();
+
+// window.scrollBy({
+//   top: cardHeight * 2,
+//   behavior: 'smooth',
+// });
